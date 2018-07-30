d8663f5d44722b8aebc4ccb9a2af3a7dd96089b8
Barbara Rzepka <bbrzepka@bgeo.es>
2018-07-30 10:56:11 +0200

    export/import csv

--- utils/fct/gw_fct_utils_pg2csv.sql
+++ utils/fct/gw_fct_utils_pg2csv.sql
@@ -7,14 +7,13 @@ This version of Giswater is provided by Giswater Association
 --FUNCTION CODE: XXXX
 
 
-CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_pg2csv(p_pg2csvcat_id integer)
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_pg2csv(p_pg2csvcat_id integer, p_path_aux text)
   RETURNS void AS
 $BODY$DECLARE
 
 v_query_text text;
 rec_table record;
-p_sys_csv2pg_config record;
-schemat text;
+
 
 BEGIN
 
@@ -29,29 +28,37 @@ BEGIN
     FOR rec_table IN SELECT * FROM sys_csv2pg_config WHERE pg2csvcat_id=p_pg2csvcat_id
      LOOP
   -- insert header
-  v_query_text = 'INSERT INTO temp_csv2pg(csv2pgcat_id,csv1) VALUES ('||p_pg2csvcat_id||','''|| rec_table.header_text||''');';
-  EXECUTE v_query_text;
+        v_query_text = 'INSERT INTO temp_csv2pg(csv2pgcat_id,csv1) VALUES ('||p_pg2csvcat_id||','''|| rec_table.header_text||''');';
+        EXECUTE v_query_text;
 
-  INSERT INTO temp_csv2pg (csv2pgcat_id,csv1) VALUES (p_pg2csvcat_id,';');
+        INSERT INTO temp_csv2pg (csv2pgcat_id,csv1) VALUES (p_pg2csvcat_id,';');
   --EXECUTE v_query_text;
   
-  INSERT INTO temp_csv2pg (csv2pgcat_id,csv1,csv2,csv3,csv4,csv5,csv6,csv7,csv8,csv9,csv10,csv11,csv12) SELECT p_pg2csvcat_id,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12
-  FROM crosstab('SELECT table_name::text,  data_type::text, column_name::text FROM information_schema.columns WHERE table_schema =''SCHEMA_NAME'' and table_name='''||rec_table.tablename||'''::text') 
-  AS rpt(table_name text, c1 text, c2 text, c3 text, c4 text, c5 text, c6 text, c7 text, c8 text, c9 text, c10 text, c11 text, c12 text);
+        INSERT INTO temp_csv2pg (csv2pgcat_id,csv1,csv2,csv3,csv4,csv5,csv6,csv7,csv8,csv9,csv10,csv11,csv12) 
+        SELECT p_pg2csvcat_id,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12
+        FROM crosstab('SELECT table_name::text,  data_type::text, column_name::text FROM information_schema.columns WHERE table_schema =''SCHEMA_NAME'' and table_name='''||rec_table.tablename||'''::text') 
+        AS rpt(table_name text, c1 text, c2 text, c3 text, c4 text, c5 text, c6 text, c7 text, c8 text, c9 text, c10 text, c11 text, c12 text);
 
-  INSERT INTO temp_csv2pg (csv2pgcat_id,csv1) VALUES (p_pg2csvcat_id,';');
+        INSERT INTO temp_csv2pg (csv2pgcat_id,csv1) VALUES (p_pg2csvcat_id,';'); 
 
   -- insert values
-  v_query_text = 'INSERT INTO temp_csv2pg SELECT nextval(''SCHEMA_NAME.temp_csv2pg_id_seq''::regclass),4,current_user,*  FROM '||rec_table.tablename;
-  EXECUTE v_query_text;
+        v_query_text = 'INSERT INTO temp_csv2pg SELECT nextval(''SCHEMA_NAME.temp_csv2pg_id_seq''::regclass),4,current_user,*  FROM '||rec_table.tablename;
+        EXECUTE v_query_text;
       
-     END LOOP;
+    END LOOP;
 
+  --export to csv
+  --v_query_text='COPY SCHEMA_NAME.temp_csv2pg(csv1) FROM '''||path_aux||''' WITH (FORMAT text);';
+      v_query_text='COPY (SELECT csv1,csv2,csv3,csv4,csv5,csv6,csv7,csv8,csv9,csv10,csv11,csv12 FROM SCHEMA_NAME.temp_csv2pg) 
+      TO '''||p_path_aux||'''DELIMITER '||''' '''||' CSV;';
+      EXECUTE v_query_text;
     RETURN;
         
 END;$BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
+
+
 
 
 
---
+++ utils/gw_fct_import_temp_csv2pg.sql
@@ -0,0 +1,50 @@
+
+	CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_import_temp_csv2pg(
+		csv2pgcat_id_aux integer,
+		path_aux text)
+	  RETURNS integer AS
+	$BODY$
+	DECLARE
+
+
+	rec_column record;
+	v_query_text text;
+
+	BEGIN
+
+	--  Search path
+		SET search_path = "SCHEMA_NAME", public;
+
+			v_query_text='COPY SCHEMA_NAME.temp_csv2pg(csv1) FROM '''||path_aux||''' WITH (FORMAT text);';
+			execute v_query_text;
+			
+			update SCHEMA_NAME.temp_csv2pg set csv2pgcat_id=8,csv1=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',1),
+			csv2=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',2),csv3=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',3),
+			csv4=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',4),csv5=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',5),
+			csv6=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',6),csv7=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',7),
+			csv8=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',8),csv9=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',9),
+			csv10=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',10),csv11=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',11),
+			csv12=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',12),csv13=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',13),
+			csv14=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',14),csv15=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',15),
+			csv16=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',16),csv17=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',17),
+			csv18=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',18),csv19=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',19),
+			csv20=split_part(trim(regexp_replace(csv1, '\s+', ' ', 'g')),' ',20);
+
+
+			FOR rec_column IN SELECT column_name FROM information_schema.columns WHERE table_schema = 'SCHEMA_NAME' AND table_name   = 'temp_csv2pg' and (column_name ilike 'csv_' or column_name ilike 'csv__')
+			LOOP
+				v_query_text='UPDATE temp_csv2pg SET '||rec_column.column_name||'= NULL WHERE '||rec_column.column_name||'='''';';
+				execute v_query_text;
+			
+			END LOOP;
+
+
+	RETURN 0;
+		
+		
+	END;
+	$BODY$
+	  LANGUAGE plpgsql VOLATILE
+	  COST 100;
+	ALTER FUNCTION SCHEMA_NAME.gw_fct_import_temp_csv2pg(integer, text)
+	  OWNER TO postgres;
