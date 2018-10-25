a65ad05fb71d2cd6e0bbf004a1c7848285ff551f
Barbara Rzepka <bbrzepka@bgeo.es>
2018-10-25 14:59:35 +0200

    ui_ve triggers

--- utils/trg/gw_trg_ui_doc.sql
+++ utils/trg/gw_trg_ui_doc.sql
@@ -19,6 +19,18 @@ BEGIN
     doc_table:= TG_ARGV[0];
 
     IF TG_OP = 'INSERT' THEN
+        IF doc_table = 'doc_x_arc' THEN
+            INSERT INTO doc_x_arc (arc_id,doc_id) VALUES (NEW.arc_id,NEW.doc_id);
+        ELSIF doc_table = 'doc_x_node' THEN
+            INSERT INTO doc_x_node (node_id,doc_id)  VALUES (NEW.node_id,NEW.doc_id);
+        ELSIF doc_table = 'doc_x_connec' THEN
+            INSERT INTO doc_x_connec (connec_id,doc_id)  VALUES (NEW.connec_id,NEW.doc_id);
+        ELSIF doc_table = 'doc_x_gully' THEN
+            INSERT INTO doc_x_gully (gully_id,doc_id)  VALUES (NEW.gully_id,NEW.doc_id);
+        ELSIF doc_table = 'doc_x_visit' THEN
+            INSERT INTO doc_x_gully (visit_id,doc_id)  VALUES (NEW.visit_id,NEW.doc_id);
+         -- PERFORM audit_function(1); 
+        END IF;
         --	PERFORM audit_function(1); 
         RETURN NEW;
 
@@ -27,13 +39,13 @@ BEGIN
         RETURN NEW;
 
     ELSIF TG_OP = 'DELETE' THEN
-        v_sql:= 'DELETE FROM '||doc_table||' WHERE id = '||quote_literal(OLD.id)||';';
-        EXECUTE v_sql;
-        --	PERFORM audit_function(3); 
-        RETURN NULL;
-    
+
+            v_sql:= 'DELETE FROM '||doc_table||' WHERE id = '||quote_literal(OLD.id)||';';
+            EXECUTE v_sql;  
+
+        RETURN NULL; 
+          --    PERFORM audit_function(3); 
     END IF;
-    
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
@@ -55,4 +67,8 @@ FOR EACH ROW EXECUTE PROCEDURE "SCHEMA_NAME".gw_trg_ui_doc(doc_x_connec);
 DROP TRIGGER IF EXISTS gw_trg_ui_doc_x_visit ON "SCHEMA_NAME".ve_ui_doc_x_visit;
 CREATE TRIGGER gw_trg_ui_doc_x_visit INSTEAD OF INSERT OR DELETE OR UPDATE ON "SCHEMA_NAME".ve_ui_doc_x_visit
 FOR EACH ROW EXECUTE PROCEDURE "SCHEMA_NAME".gw_trg_ui_doc(doc_x_visit);
+      
+DROP TRIGGER IF EXISTS gw_trg_ui_doc ON "SCHEMA_NAME".ve_ui_doc;
+CREATE TRIGGER gw_trg_ui_doc INSTEAD OF INSERT OR DELETE OR UPDATE ON "SCHEMA_NAME".ve_ui_doc
+FOR EACH ROW EXECUTE PROCEDURE "SCHEMA_NAME".gw_trg_ui_doc(doc);
       
