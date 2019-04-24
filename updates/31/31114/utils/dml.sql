1c928dec1f66908f69a476fa86ac249aaf57cf8f
Xavier Torret <xtorret@bgeo.es>
2019-04-24 19:08:18 +0200

    Enhance state=1 connect's on links

--- updates/31/31114/utils/dml.sql
+++ updates/31/31114/utils/dml.sql
@@ -11,3 +11,8 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 UPDATE audit_cat_table SET sys_role_id='role_om' WHERE id='v_ui_om_visitman_x_arc';
 UPDATE audit_cat_table SET sys_role_id='role_om' WHERE id='v_ui_om_visitman_x_node';
 UPDATE audit_cat_table SET sys_role_id='role_om' WHERE id='v_ui_om_visitman_x_connec';
+
+
+INSERT INTO audit_cat_error VALUES (2015, 'There is no state-1 feature as endpoint of link. It is impossible to create it', 'Try to connect the link to one arc / node / connec / gully or vnode with state=1', 2, true, NULL);
+
+INSERT INTO audit_cat_error 
