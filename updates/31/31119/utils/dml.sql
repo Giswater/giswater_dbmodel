54db13d8dd4d332deca4e56732abf3fbbdf0ed78
Xavier Torret <xtorret@bgeo.es>
2019-05-17 01:23:58 +0200

    Update delta files

--- updates/31/31119/utils/dml.sql
+++ updates/31/31119/utils/dml.sql
@@ -12,4 +12,5 @@ INSERT INTO audit_cat_error VALUES (1081,'There are not psectors defined on the 
 INSERT INTO audit_cat_error VALUES (1083,'Please configure your own psector vdefault variable','To work with planified elements it is mandatory to have always defined the work psector using the psector vdefault variable', 2, TRUE);
 INSERT INTO audit_cat_error VALUES (1097,'It is not allowed to insert/update one node with state(1) over another one with state (1) also. The node is:','Please ckeck it', 2, TRUE);
 
-INSERT INTO value_state_type VALUES (99, 2, 'Ficticius', true, true);
+INSERT INTO value_state_type VALUES (99, 2, 'Ficticius', true, true)
+ON CONFLICT (id) DO NOTHING;
