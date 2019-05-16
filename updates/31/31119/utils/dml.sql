f93c6ebde1794d41081fbb248a31faf4abb54af9
Xavier Torret <xtorret@bgeo.es>
2019-05-16 15:48:24 +0200

    Update ficticius values

--- updates/31/31119/utils/dml.sql
+++ updates/31/31119/utils/dml.sql
@@ -11,3 +11,5 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 INSERT INTO audit_cat_error VALUES (1081,'There are not psectors defined on the project','Define at least one to start to work with', 2, TRUE);
 INSERT INTO audit_cat_error VALUES (1083,'Please configure your own psector vdefault variable','To work with planified elements it is mandatory to have always defined the work psector using the psector vdefault variable', 2, TRUE);
 INSERT INTO audit_cat_error VALUES (1097,'It is not allowed to insert/update one node with state(1) over another one with state (1) also. The node is:','Please ckeck it', 2, TRUE);
+
+INSERT INTO value_state_type VALUES (99, 2, 'Ficticius', true, true);
