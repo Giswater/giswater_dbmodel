8995c3489ec7a1fe37ba0b2ab866ddd4f4415099
Albert Bofill <abofill@bgeo.es>
2019-06-03 16:00:59 +0200

    Minor change on last commit

--- updates/31/31121/utils/dml.sql
+++ updates/31/31121/utils/dml.sql
@@ -8,6 +8,6 @@ This version of Giswater is provided by Giswater Association
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 --03/06/2019
-UPDATE config_param_system SET value='{"status":"TRUE" , "field":"id"}' WHERE parameter='customer_code_autofill';
-UPDATE config_param_system SET descript='If status is TRUE, when insert a new connec, customer_code will be the same as field (id or code)' WHERE parameter='customer_code_autofill';
+UPDATE config_param_system SET value='{"status":"TRUE" , "field":"code"}' WHERE parameter='customer_code_autofill';
+UPDATE config_param_system SET descript='If status is TRUE, when insert a new connec, customer_code will be the same as field (connec_id or code)' WHERE parameter='customer_code_autofill';
 
--- ws/ftrg/ws_gw_trg_edit_man_connec.sql
+++ ws/ftrg/ws_gw_trg_edit_man_connec.sql
@@ -198,13 +198,13 @@ BEGIN
 	    END IF;
 
 	    -- Customer code  
-		IF NEW.customer_code IS NULL AND (SELECT (value::json->>'status')::boolean FROM config_param_system WHERE parameter = 'customer_code_autofill') IS TRUE 
+		IF NEW.customer_code IS NULL AND (SELECT (value::json->>'status')::boolean FROM config_param_system WHERE parameter = 'customer_code_autofill') IS TRUE
 		AND (SELECT (value::json->>'field')::text FROM config_param_system WHERE parameter = 'customer_code_autofill')='code'  THEN
 		
 			NEW.customer_code = NEW.code;
 			
 		ELSIF NEW.customer_code IS NULL AND (SELECT (value::json->>'status')::boolean FROM config_param_system WHERE parameter = 'customer_code_autofill') IS TRUE 
-		AND (SELECT (value::json->>'field')::text FROM config_param_system WHERE parameter = 'customer_code_autofill')='id'  THEN
+		AND (SELECT (value::json->>'field')::text FROM config_param_system WHERE parameter = 'customer_code_autofill')='connec_id'  THEN
 
 			NEW.customer_code = NEW.connec_id;
 
