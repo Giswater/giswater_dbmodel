25894162f0a6195334822b5fb2ed2d3d041816f7
Albert Bofill <abofill@bgeo.es>
2019-06-27 17:33:59 +0200

    Modify constraints of price tables

--- updates/31/31122/utils/tablect.sql
+++ updates/31/31122/utils/tablect.sql
@@ -6,9 +6,16 @@ This version of Giswater is provided by Giswater Association
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
+-- DROP
+ALTER TABLE audit_price_simple DROP CONSTRAINT audit_price_simple_pkey;
+ALTER TABLE price_compost_value DROP CONSTRAINT price_compost_value_simple_id_fkey;
+
+
+-- ADD
 ALTER TABLE price_compost ADD CONSTRAINT price_compost_pricecat_id_fkey FOREIGN KEY (pricecat_id)
 REFERENCES price_cat_simple (id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE RESTRICT;
 
-ALTER TABLE audit_price_simple DROP CONSTRAINT audit_price_simple_pkey;
-
 ALTER TABLE audit_price_simple ADD CONSTRAINT audit_price_simple_pkey PRIMARY KEY (id, pricecat_id);
+
+ALTER TABLE price_compost_value ADD CONSTRAINT price_compost_value_compost_id_fkey FOREIGN KEY (simple_id)
+REFERENCES price_compost (id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE RESTRICT;
