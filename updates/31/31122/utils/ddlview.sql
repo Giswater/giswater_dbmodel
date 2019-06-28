62ff5b16984ab65ce784987c1899e09840c22734
Xavier Torret - BGEO <xtorret@bgeo.es>
2019-06-28 10:51:52 +0200

    Bug fix on sql file

--- updates/31/31122/utils/ddlview.sql
+++ updates/31/31122/utils/ddlview.sql
@@ -16,7 +16,7 @@ SELECT price_compost.id,
            WHEN price_compost.price IS NOT NULL THEN price_compost.price::numeric(14,2)
            ELSE  sum(a.price * price_compost_value.value)::numeric(14,2)
        END AS price
-  FROM wprice_compost
+  FROM price_compost
     LEFT JOIN price_compost_value ON price_compost.id::text = price_compost_value.compost_id::text
     LEFT JOIN price_compost a ON a.id::text = price_compost_value.simple_id::text
  GROUP BY price_compost.id, price_compost.unit, price_compost.descript;
