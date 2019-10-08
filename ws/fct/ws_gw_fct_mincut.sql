87143be20d7a191c600f88ddb725cc944041f483
Xavier Torret <xtorret@bgeo.es>
2019-10-08 15:42:01 +0200

    Bug fix on JOIN / LEFT JOIN for v_rtc_hydrometer

--- ws/fct/ws_gw_fct_mincut.sql
+++ ws/fct/ws_gw_fct_mincut.sql
@@ -295,7 +295,7 @@ BEGIN
 		INSERT INTO anl_mincut_result_hydrometer (result_id, hydrometer_id)
 		SELECT result_id_arg,rtc_hydrometer_x_connec.hydrometer_id FROM rtc_hydrometer_x_connec 
 		JOIN anl_mincut_result_connec ON rtc_hydrometer_x_connec.connec_id=anl_mincut_result_connec.connec_id 
-		JOIN v_rtc_hydrometer ON v_rtc_hydrometer.hydrometer_id=rtc_hydrometer_x_connec.hydrometer_id
+		LEFT JOIN v_rtc_hydrometer ON v_rtc_hydrometer.hydrometer_id=rtc_hydrometer_x_connec.hydrometer_id
 		WHERE result_id=result_id_arg;
 
 		-- Working with the case of study of mincut pipe hazard analysis  (om_mincut_analysis_pipehazard=true)
