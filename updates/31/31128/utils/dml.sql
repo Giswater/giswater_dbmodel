e1bf58b8e703d89343a7f573fefbcf59e5be67e0
Xavier Torret <xtorret@bgeo.es>
2019-10-22 09:18:56 +0200

    Update sql files

--- updates/31/31128/utils/dml.sql
+++ updates/31/31128/utils/dml.sql
@@ -10,15 +10,13 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 -- config (warning)
 INSERT INTO config_param_system(parameter, value, data_type, context, descript)
-VALUES ('plan_psector_statetype', '{"done_planified":"9", "done_ficticious":"18", "canceled_planified":"17", "canceled_ficticious":"18"}', 'json', 'plan', 
+VALUES ('plan_psector_statetype', '{"done_planified":"98", "done_ficticious":"97", "canceled_planified":"96", "canceled_ficticious":"95"}', 'json', 'plan', 
 'Psector statetype assigned to features after executing or canceling planification');
 
 INSERT INTO config_param_system(parameter, value, data_type, context, descript)
-VALUES ('plan_statetype_planned', '11', 'integer', 'plan', 'State type for planned elements');
+VALUES ('plan_statetype_planned', '3', 'integer', 'plan', 'State type for planned elements');
 
-UPDATE config_param_system SET value='15' WHERE parameter='plan_statetype_ficticius';
-
-
+UPDATE config_param_system SET value='99' WHERE parameter='plan_statetype_ficticius';
 
 
 INSERT INTO audit_cat_error(id, error_message, log_level, show_user, project_type)
@@ -52,3 +50,19 @@ ON CONFLICT (id) DO NOTHING;
 INSERT INTO audit_cat_error(id, error_message,hint_message, log_level, show_user, project_type)
 VALUES (3036, 'Selected state type doesn''t correspond with state','Modify the value of state or state type.', 2, true,'utils')
 ON CONFLICT (id) DO NOTHING;
+
+
+INSERT INTO value_state_type(id,state, name, is_operative, is_doable)
+VALUES (99,2, 'FICTICIOUS',true, false);
+
+INSERT INTO value_state_type(id,state, name, is_operative, is_doable)
+VALUES (98,0, 'DONE PLANIFIED',false, false);
+
+INSERT INTO value_state_type(id,state, name, is_operative, is_doable)
+VALUES (97,0, 'DONE FICTICIOUS',false, false);
+
+INSERT INTO value_state_type(id,state, name, is_operative, is_doable)
+VALUES (96,0, 'CANCELED PLANIFIED',false, false);
+
+INSERT INTO value_state_type(id,state, name, is_operative, is_doable)
+VALUES (95,0, 'CANCELED FICTICIOUS',false, false);
