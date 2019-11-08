c6f493557ece5c2c3544a37b3b3ccadb09126c76
Xavier Torret <xtorret@bgeo.es>
2019-11-08 22:45:20 +0100

    Update sql file

--- updates/31/31130/review_data_ud.sql
+++ updates/31/31130/review_data_ud.sql
@@ -4,4 +4,15 @@
 
 SET search_path='SCHEMA_NAME', public;
 
--- check orphan polygons
+-- check orphan rows on polygon table
+
+-- check missed rows on inp tables
+SELECT arc_id FROM arc WHERE arc_id NOT IN (select arc_id from inp_conduit UNION select arc_id from inp_virtual UNION select arc_id from inp_weir 
+UNION select arc_id from inp_pump UNION select arc_id from inp_outlet UNION select arc_id from inp_orifice) AND state > 0
+
+SELECT node_id FROM node WHERE node_id NOT IN(
+select node_id from inp_junction UNION select node_id from inp_storage UNION select node_id from inp_outfall UNION select node_id from inp_divider)
+AND state >0
+
+
+-- check code null values on feature tables
--- updates/31/31130/review_data_ws.sql
+++ updates/31/31130/review_data_ws.sql
@@ -5,3 +5,15 @@
 SET search_path='SCHEMA_NAME', public;
 
 -- check orphan polygons
+
+
+-- check code null values
+
+
+-- check missed rows on inp tables
+SELECT arc_id FROM arc WHERE arc_id NOT IN (select arc_id from inp_pipe) AND state > 0
+SELECT node_id FROM node WHERE node_id NOT IN(
+select node_id from inp_shortpipe UNION select node_id from inp_valve UNION select node_id from inp_tank UNION select node_id from inp_reservoir UNION SELECT node_id from inp_inlet)
+AND state >0
+
+
