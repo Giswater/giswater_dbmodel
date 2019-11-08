c7ca510d13a72e7c017b054559ef3f0f6d4a0548
Xavier Torret <xtorret@bgeo.es>
2019-11-08 23:27:05 +0100

    Update review data files

--- updates/31/31130/review_data_ud.sql
+++ updates/31/31130/review_data_ud.sql
@@ -5,6 +5,15 @@
 SET search_path='SCHEMA_NAME', public;
 
 -- check orphan rows on polygon table
+SELECT pol_id FROM polygon EXCEPT SELECT pol_id  FROM (select pol_id from gully UNION select pol_id from man_chamber UNION select pol_id from man_netgully UNION select pol_id from man_storage UNION select pol_id from man_wwtp) a
+
+-- check code null values on feature tables
+SELECT arc_id, arccat_id, the_geom FROM arc WHERE code IS NULL 
+SELECT node_id, nodecat_id, the_geom FROM node WHERE code IS NULL
+SELECT connec_id, connecat_id, the_geom FROM connec WHERE code IS NULL
+SELECT guly_id, gratecat_id, the_geom FROM gully WHERE code IS NULL
+SELECT element_id, elementcat_id, the_geom FROM element WHERE code IS NULL
+
 
 -- check missed rows on inp tables
 SELECT arc_id FROM arc WHERE arc_id NOT IN (select arc_id from inp_conduit UNION select arc_id from inp_virtual UNION select arc_id from inp_weir 
@@ -15,4 +24,3 @@ select node_id from inp_junction UNION select node_id from inp_storage UNION sel
 AND state >0
 
 
--- check code null values on feature tables
