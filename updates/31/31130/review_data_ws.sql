6e811d490c7ad145263667ee0daa8c4dc1415dd4
Albert Bofill <abofill@bgeo.es>
2019-11-13 16:59:02 +0100

    Enhance querys for data review

--- updates/31/31130/review_data_ws.sql
+++ updates/31/31130/review_data_ws.sql
@@ -5,20 +5,28 @@
 SET search_path='SCHEMA_NAME', public;
 
 -- check orphan polygons
-SELECT pol_id FROM polygon EXCEPT SELECT pol_id FROM (select pol_id from man_register UNION select pol_id from man_tank UNION select pol_id from man_fountain) a
+SELECT pol_id FROM polygon EXCEPT SELECT pol_id FROM (select pol_id from man_register UNION select pol_id from man_tank UNION select pol_id from man_fountain) a;
 
+-- delete orphan polygons
+--DELETE FROM polygon WHERE pol_id IN (SELECT pol_id FROM polygon EXCEPT SELECT pol_id FROM (select pol_id from man_register UNION select pol_id from man_tank UNION select pol_id from man_fountain) a);
 
 -- check code null values
-SELECT arc_id, arccat_id, the_geom FROM arc WHERE code IS NULL 
-SELECT node_id, nodecat_id, the_geom FROM node WHERE code IS NULL 
-SELECT connec_id, connecat_id, the_geom FROM connec WHERE code IS NULL
-SELECT element_id, elementcat_id, the_geom FROM element WHERE code IS NULL
+SELECT arc_id, arccat_id, the_geom FROM arc WHERE code IS NULL; 
+SELECT node_id, nodecat_id, the_geom FROM node WHERE code IS NULL; 
+SELECT connec_id, connecat_id, the_geom FROM connec WHERE code IS NULL;
+SELECT element_id, elementcat_id, the_geom FROM element WHERE code IS NULL;
+
+-- update code null values to id in case is null
+/*UPDATE arc SET code=arc_id WHERE code IS NULL;
+UPDATE node SET code=node_id WHERE code IS NULL;
+UPDATE connec SET code=connec_id WHERE code IS NULL;
+UPDATE element SET code=element_id WHERE code IS NULL;*/
 
 
 -- check missed rows on inp tables
-SELECT arc_id FROM arc WHERE arc_id NOT IN (select arc_id from inp_pipe) AND state > 0
+SELECT arc_id FROM arc WHERE arc_id NOT IN (select arc_id from inp_pipe) AND state > 0;
 SELECT node_id FROM node WHERE node_id NOT IN(
 select node_id from inp_shortpipe UNION select node_id from inp_valve UNION select node_id from inp_tank UNION select node_id from inp_reservoir)
-AND state >0
+AND state >0;
 
 
