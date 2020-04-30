5fe90e9d94664c97a41332de459df09627245ecb
Albert Bofill <abofill@bgeo.es>
2020-04-30 18:30:59 +0200

    Update review tables and views

--- updates/33/33036/ud/ddlview.sql
+++ updates/33/33036/ud/ddlview.sql
@@ -7,6 +7,93 @@ This version of Giswater is provided by Giswater Association
 
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
+CREATE OR REPLACE VIEW v_node AS 
+ SELECT node.node_id,
+    node.code,
+    node.top_elev,
+    node.custom_top_elev,
+        CASE
+            WHEN node.custom_top_elev IS NOT NULL THEN node.custom_top_elev
+            ELSE node.top_elev
+        END AS sys_top_elev,
+    node.ymax,
+    node.custom_ymax,
+        CASE
+            WHEN node.custom_ymax IS NOT NULL THEN node.custom_ymax
+            ELSE node.ymax
+        END AS sys_ymax,
+    node.elev,
+    node.custom_elev,
+        CASE
+            WHEN node.elev IS NOT NULL AND node.custom_elev IS NULL THEN node.elev
+            WHEN node.custom_elev IS NOT NULL THEN node.custom_elev
+            ELSE NULL::numeric(12,3)
+        END AS sys_elev,
+    node.node_type,
+    node_type.type AS sys_type,
+    node.nodecat_id,
+	CASE
+		WHEN cat_node.matcat_id IS NULL THEN node.matcat_id
+		ELSE cat_node.matcat_id
+	END AS cat_matcat_id,
+    node.epa_type,
+    node.sector_id,
+    sector.macrosector_id,
+    node.state,
+    node.state_type,
+    node.annotation,
+    node.observ,
+    node.comment,
+    node.dma_id,
+    node.soilcat_id,
+    node.function_type,
+    node.category_type,
+    node.fluid_type,
+    node.location_type,
+    node.workcat_id,
+    node.workcat_id_end,
+    node.buildercat_id,
+    node.builtdate,
+    node.enddate,
+    node.ownercat_id,
+    node.muni_id,
+    node.postcode,
+    node.streetaxis_id,
+    node.postnumber,
+    node.postcomplement,
+    node.postcomplement2,
+    node.streetaxis2_id,
+    node.postnumber2,
+    node.descript,
+    cat_node.svg,
+    node.rotation,
+    concat(node_type.link_path, node.link) AS link,
+    node.verified,
+    node.the_geom,
+    node.undelete,
+    node.label_x,
+    node.label_y,
+    node.label_rotation,
+    node.publish,
+    node.inventory,
+    node.uncertain,
+    node.xyz_date,
+    node.unconnected,
+    dma.macrodma_id,
+    node.expl_id,
+    node.num_value,
+    node.lastupdate,
+    node.lastupdate_user,
+    node.insert_user
+   FROM node
+     JOIN v_state_node ON node.node_id::text = v_state_node.node_id::text
+     LEFT JOIN cat_node ON node.nodecat_id::text = cat_node.id::text
+     LEFT JOIN node_type ON node_type.id::text = node.node_type::text
+     LEFT JOIN dma ON node.dma_id = dma.dma_id
+     LEFT JOIN sector ON node.sector_id = sector.sector_id;
+
+	 
+
 
 CREATE OR REPLACE VIEW v_edit_node AS 
  SELECT v_node.node_id,
@@ -26,9 +113,7 @@ CREATE OR REPLACE VIEW v_edit_node AS
     v_node.node_type,
     v_node.sys_type,
     v_node.nodecat_id,
-	CASE
-	WHEN v_node.matcat_id IS NULL THEN cat_node.matcat_id
-	ELSE v_node.matcat_id END AS cat_matcat_id,
+	v_node.cat_matcat_id,
     v_node.epa_type,
     v_node.sector_id,
     v_node.macrosector_id,
@@ -95,8 +180,9 @@ CREATE OR REPLACE VIEW v_edit_connec AS
     connec_type.type AS sys_type,
     connec.private_connecat_id,
 	CASE
-	WHEN connec.matcat_id IS NULL THEN cat_connec.matcat_id
-	ELSE connec.matcat_id END AS cat_matcat_id,
+		WHEN cat_connec.matcat_id IS NULL THEN connec.matcat_id
+		ELSE cat_connec.matcat_id
+	END AS cat_matcat_id,
     connec.sector_id,
     sector.macrosector_id,
     connec.demand,
@@ -160,3 +246,206 @@ CREATE OR REPLACE VIEW v_edit_connec AS
      LEFT JOIN dma ON connec.dma_id = dma.dma_id
      LEFT JOIN sector ON connec.sector_id = sector.sector_id
      LEFT JOIN connec_type ON connec.connec_type::text = connec_type.id::text;
+	 
+	 
+
+-- REVIEW NODE
+CREATE OR REPLACE VIEW v_edit_review_node AS 
+ SELECT review_node.node_id,
+    review_node.top_elev,
+    review_node.ymax,
+    review_node.node_type,
+    review_node.matcat_id,
+    review_node.nodecat_id,
+    review_node.annotation,
+    review_node.observ,
+    review_node.expl_id,
+    review_node.the_geom,
+    review_node.field_checked,
+    review_node.is_validated
+   FROM review_node,
+    selector_expl
+  WHERE selector_expl.cur_user = "current_user"()::text AND review_node.expl_id = selector_expl.expl_id;
+
+DROP TRIGGER IF EXISTS gw_trg_edit_review_node ON v_edit_review_node;
+CREATE TRIGGER gw_trg_edit_review_node
+  INSTEAD OF INSERT OR UPDATE
+  ON v_edit_review_node
+  FOR EACH ROW
+  EXECUTE PROCEDURE gw_trg_edit_review_node();
+
+
+CREATE OR REPLACE VIEW v_edit_review_audit_node AS 
+ SELECT review_audit_node.id,
+    review_audit_node.node_id,
+    review_audit_node.old_top_elev,
+    review_audit_node.new_top_elev,
+    review_audit_node.old_ymax,
+    review_audit_node.new_ymax,
+    review_audit_node.old_node_type,
+    review_audit_node.new_node_type,
+    review_audit_node.old_matcat_id,
+    review_audit_node.new_matcat_id,
+    review_audit_node.old_nodecat_id,
+    review_audit_node.new_nodecat_id,
+    review_audit_node.annotation,
+    review_audit_node.observ,
+    review_audit_node.expl_id,
+    review_audit_node.the_geom,
+    review_audit_node.review_status_id,
+    review_audit_node.field_date,
+    review_audit_node.field_user,
+    review_audit_node.is_validated
+   FROM review_audit_node,
+    selector_expl
+  WHERE selector_expl.cur_user = "current_user"()::text AND review_audit_node.expl_id = selector_expl.expl_id AND review_audit_node.review_status_id <> 0;
+
+DROP TRIGGER IF EXISTS gw_trg_edit_review_audit_node ON v_edit_review_audit_node;
+CREATE TRIGGER gw_trg_edit_review_audit_node
+  INSTEAD OF UPDATE
+  ON v_edit_review_audit_node
+  FOR EACH ROW
+  EXECUTE PROCEDURE gw_trg_edit_review_audit_node();
+
+  
+  
+-- REVIEW ARC
+CREATE OR REPLACE VIEW v_edit_review_arc AS 
+ SELECT review_arc.arc_id,
+    review_arc.y1,
+    review_arc.y2,
+    review_arc.arc_type,
+    review_arc.matcat_id,
+    review_arc.arccat_id,
+    review_arc.annotation,
+    review_arc.observ,
+    review_arc.expl_id,
+    review_arc.the_geom,
+    review_arc.field_checked,
+    review_arc.is_validated
+   FROM review_arc,
+    selector_expl
+  WHERE selector_expl.cur_user = "current_user"()::text AND review_arc.expl_id = selector_expl.expl_id;
+
+DROP TRIGGER IF EXISTS gw_trg_edit_review_arc ON v_edit_review_arc;
+CREATE TRIGGER gw_trg_edit_review_arc
+  INSTEAD OF INSERT OR UPDATE
+  ON v_edit_review_arc
+  FOR EACH ROW
+  EXECUTE PROCEDURE gw_trg_edit_review_arc();
+  
+
+CREATE OR REPLACE VIEW v_edit_review_audit_arc AS 
+ SELECT review_audit_arc.id,
+    review_audit_arc.arc_id,
+    review_audit_arc.old_y1,
+    review_audit_arc.new_y1,
+    review_audit_arc.old_y2,
+    review_audit_arc.new_y2,
+    review_audit_arc.old_arc_type,
+    review_audit_arc.new_arc_type,
+    review_audit_arc.old_arccat_id,
+    review_audit_arc.new_arccat_id,
+    review_audit_arc.annotation,
+    review_audit_arc.observ,
+    review_audit_arc.expl_id,
+    review_audit_arc.the_geom,
+    review_audit_arc.review_status_id,
+    review_audit_arc.field_date,
+    review_audit_arc.field_user,
+    review_audit_arc.is_validated
+   FROM review_audit_arc,
+    selector_expl
+  WHERE selector_expl.cur_user = "current_user"()::text AND review_audit_arc.expl_id = selector_expl.expl_id AND review_audit_arc.review_status_id <> 0;
+
+
+DROP TRIGGER IF EXISTS gw_trg_edit_review_audit_arc ON v_edit_review_audit_arc;
+CREATE TRIGGER gw_trg_edit_review_audit_arc
+  INSTEAD OF UPDATE
+  ON v_edit_review_audit_arc
+  FOR EACH ROW
+  EXECUTE PROCEDURE gw_trg_edit_review_audit_arc();
+  
+
+-- REVIEW GULLY
+CREATE OR REPLACE VIEW v_edit_review_gully AS 
+ SELECT review_gully.gully_id,
+    review_gully.top_elev,
+    review_gully.ymax,
+    review_gully.sandbox,
+    review_gully.matcat_id,
+    review_gully.gully_type,
+    review_gully.gratecat_id,
+    review_gully.units,
+    review_gully.groove,
+    review_gully.siphon,
+    review_gully.connec_matcat_id,
+    review_gully.connec_arccat_id,
+    review_gully.featurecat_id,
+    review_gully.feature_id,
+    review_gully.annotation,
+    review_gully.observ,
+    review_gully.expl_id,
+    review_gully.the_geom,
+    review_gully.field_checked,
+    review_gully.is_validated
+   FROM review_gully,
+    selector_expl
+  WHERE selector_expl.cur_user = "current_user"()::text AND review_gully.expl_id = selector_expl.expl_id;
+
+DROP TRIGGER IF EXISTS gw_trg_edit_review_gully ON v_edit_review_gully;
+CREATE TRIGGER gw_trg_edit_review_gully
+  INSTEAD OF INSERT OR UPDATE
+  ON v_edit_review_gully
+  FOR EACH ROW
+  EXECUTE PROCEDURE gw_trg_edit_review_gully();
+
+
+CREATE OR REPLACE VIEW v_edit_review_audit_gully AS 
+ SELECT review_audit_gully.id,
+    review_audit_gully.gully_id,
+    review_audit_gully.old_top_elev,
+    review_audit_gully.new_top_elev,
+    review_audit_gully.old_ymax,
+    review_audit_gully.new_ymax,
+    review_audit_gully.old_sandbox,
+    review_audit_gully.new_sandbox,
+    review_audit_gully.old_matcat_id,
+    review_audit_gully.new_matcat_id,
+    review_audit_gully.old_gully_type,
+    review_audit_gully.new_gully_type,
+    review_audit_gully.old_gratecat_id,
+    review_audit_gully.new_gratecat_id,
+    review_audit_gully.old_units,
+    review_audit_gully.new_units,
+    review_audit_gully.old_groove,
+    review_audit_gully.new_groove,
+    review_audit_gully.old_siphon,
+    review_audit_gully.new_siphon,
+    review_audit_gully.old_connec_matcat_id,
+    review_audit_gully.new_connec_matcat_id,
+    review_audit_gully.old_connec_arccat_id,
+    review_audit_gully.new_connec_arccat_id,
+    review_audit_gully.old_featurecat_id,
+    review_audit_gully.new_featurecat_id,
+    review_audit_gully.old_feature_id,
+    review_audit_gully.new_feature_id,
+    review_audit_gully.annotation,
+    review_audit_gully.observ,
+    review_audit_gully.expl_id,
+    review_audit_gully.the_geom,
+    review_audit_gully.review_status_id,
+    review_audit_gully.field_date,
+    review_audit_gully.field_user,
+    review_audit_gully.is_validated
+   FROM review_audit_gully,
+    selector_expl
+  WHERE selector_expl.cur_user = "current_user"()::text AND review_audit_gully.expl_id = selector_expl.expl_id AND review_audit_gully.review_status_id <> 0;
+
+DROP TRIGGER IF EXISTS gw_trg_edit_review_audit_gully ON v_edit_review_audit_gully;
+CREATE TRIGGER gw_trg_edit_review_audit_gully
+  INSTEAD OF UPDATE
+  ON v_edit_review_audit_gully
+  FOR EACH ROW
+  EXECUTE PROCEDURE gw_trg_edit_review_audit_gully();
+
