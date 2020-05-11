82b9c26936393743a38d3e33e5d73ce57432a25a
Albert Bofill <abofill@bgeo.es>
2020-05-11 15:43:15 +0200

    Add ui_tablename to om_visit_class

--- updates/31/31132/utils/ddl.sql
+++ updates/31/31132/utils/ddl.sql
@@ -146,7 +146,8 @@ CREATE TABLE om_visit_class(
   sys_role_id character varying(30),
   visit_type integer,
   param_options json,
-  tablename character varying(30)
+  tablename character varying(30),
+  ui_tablename character varying(30)
   CONSTRAINT om_visit_class_pkey PRIMARY KEY (id));
   
   
