fce17fa6979907d494639be44ca8dbb64dd9768d
Xavier Torret <xtorret@bgeo.es>
2020-05-13 14:53:52 +0200

    Enhance performance for mapzone analytics

---
+++ updates/33/33037/utils/dml.sql
@@ -0,0 +1,11 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+--2020/05/09
+UPDATE ws.audit_cat_table SET sys_role_id = 'role_master' WHERE id = 'config_api_form_fields';
