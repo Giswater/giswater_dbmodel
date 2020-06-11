c163c3c86171e70170a4cacef48759fb6adcd860
Xavier Torret <xtorret@bgeo.es>
2020-06-11 11:20:11 +0200

    Bug fix on multipump exportation

--- ws/fct/ws_gw_fct_pg2epa_pump_additional.sql
+++ ws/fct/ws_gw_fct_pg2epa_pump_additional.sql
@@ -37,73 +37,74 @@ v_old_arc_id varchar(16);
 
 
 BEGIN
-
---  Search path
+	
+	--  Search path
     SET search_path = "SCHEMA_NAME", public; 
 
     SELECT * INTO rec FROM version LIMIT 1;
  
---  Start process	
+	--  Start process	
     RAISE NOTICE 'Starting additional pumps process.';
 	
---  Loop for pumping stations
+	--  Loop for pumping stations
     FOR node_id_aux IN (SELECT DISTINCT node_id FROM inp_pump_additional JOIN rpt_inp_arc ON concat(node_id,'_n2a')=arc_id WHERE result_id=result_id_var)
     LOOP
 
-	SELECT * INTO arc_rec FROM rpt_inp_arc WHERE arc_id=concat(node_id_aux,'_n2a') AND result_id=result_id_var;
-	
---  	Loop for additional pumps
-	FOR pump_rec IN SELECT * FROM inp_pump_additional WHERE node_id=node_id_aux
-	LOOP
-
-		-- Id creation from pattern arc
-		v_old_arc_id = arc_rec.arc_id;
-		record_new_arc.arc_id=concat(arc_rec.arc_id,pump_rec.order_id);
-
-		-- Right or left hand
-		odd_var = pump_rec.order_id %2;
+		SELECT * INTO arc_rec FROM rpt_inp_arc WHERE arc_id=concat(node_id_aux,'_n2a') AND result_id=result_id_var;
 		
-		if (odd_var)=0 then 
-			angle=(ST_Azimuth(ST_startpoint(arc_rec.the_geom), ST_endpoint(arc_rec.the_geom)))+1.57;
-		else 
-			angle=(ST_Azimuth(ST_startpoint(arc_rec.the_geom), ST_endpoint(arc_rec.the_geom)))-1.57;
+		-- Loop for additional pumps
+		FOR pump_rec IN SELECT * FROM inp_pump_additional WHERE node_id=node_id_aux
+		LOOP
+
+			-- Id creation from pattern arc
+			v_old_arc_id = arc_rec.arc_id;
+			record_new_arc.arc_id=concat(arc_rec.arc_id,pump_rec.order_id);
+
+			-- Right or left hand
+			odd_var = pump_rec.order_id %2;
+			
+			if (odd_var)=0 then 
+				angle=(ST_Azimuth(ST_startpoint(arc_rec.the_geom), ST_endpoint(arc_rec.the_geom)))+1.57;
+			else 
+				angle=(ST_Azimuth(ST_startpoint(arc_rec.the_geom), ST_endpoint(arc_rec.the_geom)))-1.57;
+			end if;
+
 			pump_order = (pump_rec.order_id);
-		end if;
 
-		-- Copiyng values from patter arc
-		record_new_arc.node_1 = arc_rec.node_1;
-		record_new_arc.node_2 = arc_rec.node_2;
-		record_new_arc.epa_type = arc_rec.epa_type;
- 		record_new_arc.sector_id = arc_rec.sector_id;
- 		record_new_arc.state = arc_rec.state;
- 		record_new_arc.arccat_id = arc_rec.arccat_id;
- 		
-		-- Geometry construction from pattern arc
-		-- intermediate variables
-		n1_geom = ST_LineInterpolatePoint(arc_rec.the_geom, 0.100000);
-		n2_geom = ST_LineInterpolatePoint(arc_rec.the_geom, 0.900000);
-		dist = (ST_Distance(ST_transform(ST_startpoint(arc_rec.the_geom),rec.epsg), ST_LineInterpolatePoint(arc_rec.the_geom, 0.2000000))); 
+			-- Copiyng values from patter arc
+			record_new_arc.node_1 = arc_rec.node_1;
+			record_new_arc.node_2 = arc_rec.node_2;
+			record_new_arc.epa_type = arc_rec.epa_type;
+			record_new_arc.sector_id = arc_rec.sector_id;
+			record_new_arc.state = arc_rec.state;
+			record_new_arc.arccat_id = arc_rec.arccat_id;
+			
+			-- Geometry construction from pattern arc
+			-- intermediate variables
+			n1_geom = ST_LineInterpolatePoint(arc_rec.the_geom, 0.500000);
+			n2_geom = ST_LineInterpolatePoint(arc_rec.the_geom, 0.500000);
+			dist = (ST_Distance(ST_transform(ST_startpoint(arc_rec.the_geom),rec.epsg), ST_LineInterpolatePoint(arc_rec.the_geom, 0.5000000))); 
 
-		--create point1
-		yp1 = ST_y(n1_geom)-(cos(angle))*dist*0.10000*(pump_order::float);
-		xp1 = ST_x(n1_geom)-(sin(angle))*dist*0.10000*(pump_order::float);
-		raise notice' % % %', cos(angle), dist, pump_order::float;
-		
-		--create point2
-		yp2 = ST_y(n2_geom)-cos(angle)*dist*0.100000*(pump_order::float);
-		xp2 = ST_x(n2_geom)-sin(angle)*dist*0.100000*(pump_order::float);
+			--create point1
+			yp1 = ST_y(n1_geom)-(cos(angle))*dist*0.50000*(pump_order::float);
+			xp1 = ST_x(n1_geom)-(sin(angle))*dist*0.50000*(pump_order::float);
+			raise notice' % % %', cos(angle), dist, pump_order::float;
+			
+			--create point2
+			yp2 = ST_y(n2_geom)-cos(angle)*dist*0.500000*(pump_order::float);
+			xp2 = ST_x(n2_geom)-sin(angle)*dist*0.500000*(pump_order::float);
 
-		p1_geom = ST_SetSRID(ST_MakePoint(xp1, yp1),rec.epsg);	
-		p2_geom = ST_SetSRID(ST_MakePoint(xp2, yp2),rec.epsg);	
+			p1_geom = ST_SetSRID(ST_MakePoint(xp1, yp1),rec.epsg);	
+			p2_geom = ST_SetSRID(ST_MakePoint(xp2, yp2),rec.epsg);	
 
-		--create arc
-		record_new_arc.the_geom=ST_makeline(ARRAY[ST_startpoint(arc_rec.the_geom), p1_geom, p2_geom, ST_endpoint(arc_rec.the_geom)]);
+			--create arc
+			record_new_arc.the_geom=ST_makeline(ARRAY[ST_startpoint(arc_rec.the_geom), p1_geom, p2_geom, ST_endpoint(arc_rec.the_geom)]);
 
-		-- Inserting into rpt_inp_arc
-		INSERT INTO rpt_inp_arc (result_id, arc_id, node_1, node_2, arc_type, epa_type, sector_id, arccat_id, state, state_type, status, the_geom, expl_id, flw_code) 
-		VALUES (result_id_var, record_new_arc.arc_id, record_new_arc.node_1, record_new_arc.node_2, 'NODE2ARC', record_new_arc.epa_type, record_new_arc.sector_id, 
-		record_new_arc.arccat_id, record_new_arc.state, record_new_arc.state_type, pump_rec.status, record_new_arc.the_geom, record_new_arc.expl_id, v_old_arc_id);			
-	END LOOP;
+			-- Inserting into rpt_inp_arc
+			INSERT INTO rpt_inp_arc (result_id, arc_id, node_1, node_2, arc_type, epa_type, sector_id, arccat_id, state, state_type, status, the_geom, expl_id, flw_code) 
+			VALUES (result_id_var, record_new_arc.arc_id, record_new_arc.node_1, record_new_arc.node_2, 'NODE2ARC', record_new_arc.epa_type, record_new_arc.sector_id, 
+			record_new_arc.arccat_id, record_new_arc.state, record_new_arc.state_type, pump_rec.status, record_new_arc.the_geom, record_new_arc.expl_id, v_old_arc_id);			
+		END LOOP;
     
     END LOOP;
      	
@@ -115,4 +116,3 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-  
