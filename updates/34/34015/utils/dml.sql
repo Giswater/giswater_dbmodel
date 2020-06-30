b2dc0286ba2edd9ce635892968d1dbaeeff7326b
Néstor Ibáñez <nestor@bgeo.es>
2020-06-30 14:14:43 +0200

    gw_fct_pg2epa_check_data added

--- updates/34/34015/utils/dml.sql
+++ updates/34/34015/utils/dml.sql
@@ -8,7 +8,7 @@ This version of Giswater is provided by Giswater Association
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 -- 2020/06/21
-
+INSERT INTO config_function VALUES (2431, 'gw_fct_pg2epa_check_data', '{"layerName":"v_edit_arc","style":"categorized","field":"expl_id","width":2,"opacity":0.5,"values":[{"id":2,"color":[255,0,0]},{"id":1,"color":[0,2,250]}]}', '{"addToc": {"v_edit_arc": {"the_geom": "the_geom","field_id": "arc_id","group": "groupTest"}}}', null);
 INSERT INTO config_function VALUES (2112, 'gw_fct_arc_fusion', null, '{"addToc": {"v_edit_arc": {"the_geom": "the_geom","field_id": "arc_id","group": "groupTest"},
                                 "v_edit_connec": {"the_geom": "the_geom","field_id": "connec_id", "group": "groupTest"}},"active": "v_edit_arc","zoom": "v_edit_arc",
                                 "visible": ["v_edit_arc","v_edit_node"],"index": ["v_edit_arc","v_edit_node"]}', null);
