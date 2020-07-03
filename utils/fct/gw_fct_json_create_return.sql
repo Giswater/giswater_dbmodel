71f50ba1f2324265c9322ae76ff901c8823b6c60
Néstor Ibáñez <nestor@bgeo.es>
2020-07-03 18:46:04 +0200

    wip style

--- utils/fct/gw_fct_json_create_return.sql
+++ utils/fct/gw_fct_json_create_return.sql
@@ -1,10 +1,6 @@
+-- Function: ws_sample_json.gw_fct_json_create_return(json, integer)
 
-/*
-This file is part of Giswater 3
-The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-This version of Giswater is provided by Giswater Association
-*/
-
+-- DROP FUNCTION ws_sample_json.gw_fct_json_create_return(json, integer);
 
 CREATE OR REPLACE FUNCTION ws_sample_json.gw_fct_json_create_return(
     p_data json,
@@ -24,6 +20,7 @@ v_body json;
 v_returnmanager json;
 v_layermanager json;
 v_return json;
+v_qml json;
 
 
 
@@ -32,43 +29,64 @@ BEGIN
 	-- Search path
 	SET search_path = 'ws_sample_json', public;
 
-	-- get funcion specific keys
-	v_returnmanager = (SELECT returnmanager FROM config_function where id = p_fnumber);
-	v_layermanager = (SELECT layermanager FROM config_function where id = p_fnumber);
-	v_actions = (SELECT actions FROM config_function where id = p_fnumber);
-raise notice 'v_returnmanager %',v_returnmanager;
-raise notice 'v_layermanager %',v_layermanager;
-raise notice 'v_actions %',v_actions;
-	-- Control nulls
-	v_returnmanager := COALESCE(v_returnmanager, '{}');
-	v_layermanager := COALESCE(v_layermanager, '{}'); 
-	v_actions := COALESCE(v_actions, '[]'); 	
 	
-
 	-- returnmanager	
 	-- example1: v_style: {["layer":"tablename1", "mode":"Disabled", "parameters": null], ["layer":"tablename2", "mode":"basicRGB", "parameters": [R,G,B, opacidad]], ["layer":"tablename3", "mode":"qml", "parameters": {"id":4}]}
 	-- example2: v_style: {["layer":"temp_point", "mode":"Disabled", "parameters": null], ["layer":"temp_line", "mode":"BasicRGB", "parameters": [R,G,B, opacidad]], ["layer":"temp_pol", "mode":"qml", "parameters": {"id":4}]}
+	v_returnmanager = (SELECT returnmanager FROM config_function where id = p_fnumber);
 	v_body = gw_fct_json_object_set_key((p_data->>'body')::json, 'returnManager', v_returnmanager);
-	p_data = gw_fct_json_object_set_key((p_data)::json, 'body', v_body);	
+	p_data = gw_fct_json_object_set_key((p_data)::json, 'body', v_body);
 	
-
 	-- layermanager
 	-- example: v_layermanager : "layermanager":   {"addToc":{"v_edit_arc":{"the_geom":"the_geom","field_id":"arc_id","group":"groupTest"},
 	--							  "v_edit_connec":{"the_geom":"the_geom","field_id":"connec_id","group":"groupTest"}},
 	--						"active":"v_edit_arc",
 	--						"zoom":"v_edit_arc",
 	--						"visible":["v_edit_arc","v_edit_node"],"index":["v_edit_arc","v_edit_node"]}
+
+
+
+	EXECUTE 'SELECT row_to_json(row) FROM (SELECT sytelvalue as qml FROM sys_style WHERE idval='''||p_fnumber||''') row' INTO v_qml;
+raise notice 'v_qml-->%',v_qml;
+	v_layermanager = (SELECT layermanager FROM config_function where id = p_fnumber);
+	raise notice 'v_layermanager-->%',v_layermanager;
+	v_layermanager = gw_fct_json_object_set_key((v_layermanager)::json, 'style', v_qml->>'qml');
+	--v_layermanager = gw_fct_json_object_set_key((v_layermanager)::json, 'zzzz', v_qml->>'qml');
+	
 	v_body = gw_fct_json_object_set_key((p_data->>'body')::json, 'layerManager', v_layermanager);
 	p_data = gw_fct_json_object_set_key((p_data)::json, 'body', v_body);
 	
-	-- actions
-	-- example1: v_actions : '["set_sytle_mapzones"]';
-	v_body = gw_fct_json_object_set_key((p_data->>'body')::json, 'actions', v_actions);
+	--v_body = gw_fct_json_object_set_key(((p_data->>'body')::json->>'layerManager')::json, 'qml', v_qml->>'qml');
+	
+
+	raise notice 'v_body 1---->%',v_body;
+	
+	raise notice 'p_data 2---->%',p_data;
+	
+raise notice 'v_body 3---->%',v_body;
+raise notice 'p_data 3---->%',p_data;
 	p_data = gw_fct_json_object_set_key((p_data)::json, 'body', v_body);
+raise notice 'p_data 4---->%',p_data;
+
+
+	
+	v_actions = (SELECT actions FROM config_function where id = p_fnumber);
+
+	-- Control nulls
+	v_returnmanager := COALESCE(v_returnmanager, '{}');
+	v_layermanager := COALESCE(v_layermanager, '{}'); 
+	v_actions := COALESCE(v_actions, '[]'); 	
 	
 
 	
-	raise notice 'p_data-->%',p_data;
+	
+
+	-- actions
+	-- example1: v_actions : '["set_sytle_mapzones"]';
+	--v_body = gw_fct_json_object_set_key((p_data->>'body')::json, 'actions', v_actions);
+	--p_data = gw_fct_json_object_set_key((p_data)::json, 'body', v_body);
+
+	
 
 	 
 	-- Return
