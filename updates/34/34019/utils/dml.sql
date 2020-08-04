9bed1871ea2cf9dcf36ba1e5e1a5e00f5b256b2c
Xavier Torret <xtorret@bgeo.es>
2020-08-04 17:46:38 +0200

    New function to control vnode inconsistency (link without vnode or vnode without link)

--- updates/34/34019/utils/dml.sql
+++ updates/34/34019/utils/dml.sql
@@ -9,3 +9,7 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 -- 2020/07/14
 UPDATE config_param_system SET project_type = 'ws' WHERE parameter = 'om_mincut_enable_alerts';
+
+--2020/08/04
+INSERT INTO audit_cat_function (id, function_name, project_type, function_type) 
+VALUES (2994, 'gw_fct_vnode_repair', 'utils', 'function')ON CONFLICT (id) DO NOTHING;
