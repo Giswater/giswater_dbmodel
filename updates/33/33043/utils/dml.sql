b9014bee51efc1f2bbcd1299e59661faa4fc7905
Albert Bofill <abofill@bgeo.es>
2020-09-03 13:46:53 +0200

    Set om_param_type_vdefault enabled and disable qgis_qml widgets on config

--- updates/33/33043/utils/dml.sql
+++ updates/33/33043/utils/dml.sql
@@ -11,3 +11,6 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 --2020/08/04
 INSERT INTO audit_cat_function (id, function_name, project_type, function_type) 
 VALUES (2994, 'gw_fct_vnode_repair', 'utils', 'function')ON CONFLICT (id) DO NOTHING;
+
+UPDATE audit_cat_param_user SET isenabled=TRUE, layout_order=10 WHERE id='om_param_type_vdefault';
+UPDATE audit_cat_param_user SET isenabled=FALSE WHERE id like'qgis_qml%';
