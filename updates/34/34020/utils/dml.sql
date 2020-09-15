427f5b20251b6d031641c30985c952f35ba95abc
Barbara <bbrzepka@bgeo.es>
2020-09-15 14:21:29 +0200

    Save fprocess code for future development

--- updates/34/34020/utils/dml.sql
+++ updates/34/34020/utils/dml.sql
@@ -56,3 +56,53 @@ INSERT INTO config_toolbox VALUES (2496, 'Arc repair', TRUE, '{"featureType":["a
 
 INSERT INTO config_toolbox VALUES (2522, 'Import epanet inp file', TRUE, '{"featureType":[]}', '[{"widgetname":"useNode2arc", "label":"Create node2arc:", "widgettype":"check","datatype":"boolean","layoutname":"grl_option_parameters","layoutorder":1,"value":"false"}]', null, TRUE)  ON CONFLICT (id) DO NOTHING;
 INSERT INTO config_toolbox VALUES (2524, 'Import swmm inp file', TRUE, '{"featureType":[]}', '[{"widgetname":"createSubcGeom", "label":"Create subcatchments geometry:", "widgettype":"check","datatype":"boolean","layoutname":"grl_option_parameters","layoutorder":1,"value":"true"}]', null, TRUE)  ON CONFLICT (id) DO NOTHING;
+
+
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (251, 'Conduits with negative slope and inverted slope','ud') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (252, 'Features state=2 are involved in psector','utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (253, 'State not according with state_type','utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (254, 'Features with code null','utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (255, 'Orphan polygons', 'utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (256, 'Orphan rows on addfields values', 'utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (257, 'Connec or gully without or with wrong arc_id', 'utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (258, 'Vnode inconsistency - link without vnode', 'utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (259, 'Vnode inconsistency - vnode without link', 'utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (260, 'Link without feature_id', 'utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (261, 'Link without exit_id', 'utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (262, 'Features state=1 and end date', 'utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (263, 'Features state=0 without end date', 'utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (264, 'Features state=1 and end date before start date', 'utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (265, 'Automatic links with more than 100m', 'utils') ON CONFLICT (fid) DO NOTHING;
+
+INSERT INTO sys_fprocess (fid, fprocess_name, project_type)
+VALUES (266, 'Duplicated ID between arc, node, connec, gully', 'utils') ON CONFLICT (fid) DO NOTHING;
