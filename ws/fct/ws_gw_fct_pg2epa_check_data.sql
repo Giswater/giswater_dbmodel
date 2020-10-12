94bd126282f672a6c04b590becadd92d279ba1f0
Xavier Torret <xtorret@bgeo.es>
2020-10-12 11:08:59 +0200

    Minor spelling details for check data function

--- ws/fct/ws_gw_fct_pg2epa_check_data.sql
+++ ws/fct/ws_gw_fct_pg2epa_check_data.sql
@@ -95,7 +95,7 @@ BEGIN
 		EXECUTE concat ('INSERT INTO anl_node (fid, node_id, nodecat_id, descript, the_geom) SELECT 107, node_id, nodecat_id, ''Orphan node'',
 		the_geom FROM ', v_querytext);
 		INSERT INTO audit_check_data (fid, criticity, error_message)
-		VALUES (v_fid, 3, concat('ERROR: ',v_count,' node''s orphans with epa_type and state_type ready to work, will not exported because any arcs are connected. Take a look on temporal for details.'));
+		VALUES (v_fid, 2, concat('WARNING: ',v_count,' node''s orphans with epa_type and state_type ready to work, will not exported because any arcs are connected. Take a look on temporal for details.'));
 	ELSE
 		INSERT INTO audit_check_data (fid, criticity, error_message)
 		VALUES (v_fid, 1, 'INFO: No node(s) orphan found.');
