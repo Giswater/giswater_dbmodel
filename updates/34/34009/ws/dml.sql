e3e1caed4da1de1125045433aa3965fc62fdc2eb
Albert Bofill <abofill@bgeo.es>
2020-10-16 09:15:57 +0200

    Control mincut cause on updates

--- updates/34/34009/ws/dml.sql
+++ updates/34/34009/ws/dml.sql
@@ -10,9 +10,19 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 -- 2020/05/09
 UPDATE sys_table SET sys_sequence = null, sys_sequence_field = null WHERE id LIKE '%selector%';
 
+-- make sure to on next INSERT id field will be the same as descript
+UPDATE _anl_mincut_cat_cause_ SET id=descript;
+
+ALTER TABLE om_mincut DROP CONSTRAINT anl_mincut_result_cat_mincut_state_fkey;
+ALTER TABLE om_mincut DROP CONSTRAINT anl_mincut_result_cat_mincut_class_fkey;
+ALTER TABLE om_mincut DROP CONSTRAINT anl_mincut_result_cat_cause_anl_cause_fkey;
+
 INSERT INTO om_typevalue SELECT 'mincut_cause', row_number() over (order by id), descript FROM _anl_mincut_cat_cause_;
 INSERT INTO om_typevalue SELECT 'mincut_class', id, name FROM _anl_mincut_cat_class_;
 INSERT INTO om_typevalue SELECT 'mincut_state', id, name FROM _anl_mincut_cat_state_;
+
+--update mincut_cause from old idval to new numeric id
+UPDATE om_mincut m SET anl_cause=t.id FROM om_typevalue t WHERE t.idval=m.anl_cause AND typevalue='mincut_cause';
 
 SELECT setval('sys_foreingkey_id_seq', (SELECT max(id) FROM sys_foreignkey));
 
@@ -21,6 +31,4 @@ INSERT INTO sys_foreignkey (typevalue_table, typevalue_name, target_table, targe
 INSERT INTO sys_foreignkey (typevalue_table, typevalue_name, target_table, target_field) VALUES ('om_typevalue','mincut_state','om_mincut','mincut_state');
 
 
-ALTER TABLE om_mincut DROP CONSTRAINT anl_mincut_result_cat_mincut_state_fkey;
-ALTER TABLE om_mincut DROP CONSTRAINT anl_mincut_result_cat_mincut_class_fkey;
-ALTER TABLE om_mincut DROP CONSTRAINT anl_mincut_result_cat_cause_anl_cause_fkey;
+
