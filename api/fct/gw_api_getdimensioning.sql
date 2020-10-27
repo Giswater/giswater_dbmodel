f7023386be8007d6d43b9fae55ee25a82e2e3c4c
Néstor Ibáñez <nestor@bgeo.es>
2020-10-27 09:11:50 +0100

    Use coordinates to get exploitation

--- api/fct/gw_api_getdimensioning.sql
+++ api/fct/gw_api_getdimensioning.sql
@@ -12,10 +12,10 @@ $BODY$
 
 /*EXAMPLE
 SELECT SCHEMA_NAME.gw_api_getdimensioning($${
-		"client":{"device":9, "infoType":100, "lang":"ES"},
-		"form":{},
-		"feature":{},
-		"data":{}}$$)
+    "client":{"device":4, "infoType":1, "lang":"ES"}, 
+    "form":{}, "feature":{}, 
+    "data":{"filterFields":{}, "pageInfo":{}, 
+        "coordinates":{"x1":418482.9909557662, "y1":4577973.008625593, "x2":418513.4491390207, "y2":4577971.030821485}}}$$)
 */
 
 DECLARE
@@ -36,6 +36,14 @@ DECLARE
 	v_fields json;
 	field json;
 	v_id int8;
+    v_expl integer;
+	v_epsg integer;
+	v_input_geometry public.geometry;
+	v_x1 double precision;
+	v_y1 double precision;
+	v_x2 double precision;
+	v_y2 double precision;
+	count_aux integer;
 
 
 BEGIN
@@ -46,7 +54,9 @@ BEGIN
 	SET search_path = "SCHEMA_NAME", public;
 	schemas_array := current_schemas(FALSE);
 
-	
+-- 	Get srid
+	v_epsg = (SELECT epsg FROM version LIMIT 1);
+
 --      Get values from config
 	EXECUTE 'SELECT row_to_json(row) FROM (SELECT value FROM config_param_system WHERE parameter=''ApiVersion'') row'
 		INTO v_apiversion;
@@ -61,7 +71,29 @@ BEGIN
 	v_id = (SELECT nextval('SCHEMA_NAME.dimensions_id_seq'::regclass));
 
 	v_featureinfo = '{"tableName":"v_edit_dimensions", "idName":"id", "id":'||v_id||'}';
+    
+--	getting input data 
+	v_x1 := (((p_data ->>'data')::json->>'coordinates')::json->>'x1')::float;
+	v_y1 := (((p_data ->>'data')::json->>'coordinates')::json->>'y1')::float;
+	v_x2 := (((p_data ->>'data')::json->>'coordinates')::json->>'x2')::float;
+	v_y2 := (((p_data ->>'data')::json->>'coordinates')::json->>'y2')::float;
+    
+ --	Geometry column
+	IF v_x2 IS NULL THEN
+	v_input_geometry:= ST_SetSRID(ST_MakePoint(v_x1, v_y1),v_epsg);
+	ELSIF v_x2 IS NOT NULL THEN
+	v_input_geometry:= ST_SetSRID(ST_MakeLine(ST_MakePoint(v_x1, v_y1), ST_MakePoint(v_x2, v_y2)), v_epsg);
+	END IF;
 
+--	USE reduced geometry to intersect with expl mapzone in order to enhance the selectedId expl
+	SELECT count(*) into count_aux FROM exploitation WHERE ST_DWithin(v_input_geometry, exploitation.the_geom,0.001);
+	IF count_aux = 1 THEN
+		v_expl = (SELECT expl_id FROM exploitation WHERE ST_DWithin(v_input_geometry, exploitation.the_geom,0.001)  AND active=true LIMIT 1);
+	ELSE
+		SELECT expl_id INTO v_expl FROM selector_expl WHERE cur_user = current_user LIMIT 1;
+	END IF;
+	
+    
 	EXECUTE 'SELECT gw_api_get_formfields($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)'
 	INTO v_fields_array
 	USING 'v_edit_dimensions', 'feature', '', NULL, NULL, NULL, NULL, 'SELECT', null, 3;
