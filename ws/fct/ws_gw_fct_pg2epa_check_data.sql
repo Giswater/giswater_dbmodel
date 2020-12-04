7b4a492a8b195b28cbfbefe2f13739d1fbc0c46b
Xavier Torret <xtorret@bgeo.es>
2020-12-04 18:37:15 +0100

    Enhance check dint nulls for cat_node's only when this cat_node is shortpipe/valve defined as epa_type on network

--- ws/fct/ws_gw_fct_pg2epa_check_data.sql
+++ ws/fct/ws_gw_fct_pg2epa_check_data.sql
@@ -483,9 +483,9 @@ BEGIN
 		INSERT INTO audit_check_data (fid, result_id, criticity, error_message)
 		VALUES (v_fid, v_result_id, 1, 'INFO: Dint for arc''s catalog checked. No values missed.');
 	END IF;
-	
-	
-	SELECT count(*) INTO v_count FROM cat_node WHERE dint IS NULL;
+
+
+	SELECT count(*) INTO v_count FROM cat_node WHERE dint IS NULL AND id IN (SELECT DISTINCT(nodecat_id) from v_edit_node WHERE epa_type IN ('SHORTPIPE', 'VALVE'));
 	IF v_count > 0 THEN
 		INSERT INTO audit_check_data (fid, result_id, criticity, error_message)
 		VALUES (v_fid, v_result_id, 2, concat(
