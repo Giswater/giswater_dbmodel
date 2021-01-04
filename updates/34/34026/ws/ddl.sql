b20f96cb9008f5f80eaf3d8fb46a29a14eb255f5
Xavier Torret <xtorret@bgeo.es>
2021-01-04 16:46:28 +0100

    Improve performance of mincut (huge enhancement)

---
+++ updates/34/34026/ws/ddl.sql
@@ -0,0 +1,17 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = ws_sample, public, pg_catalog;
+
+
+-- 2021/01/04
+CREATE TABLE IF NOT EXISTS temp_mincut (
+id bigserial PRIMARY KEY,
+source int8,
+target int8,
+cost int2,
+reverse_cost int2);
