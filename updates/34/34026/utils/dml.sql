23819c9f67bd7320d6c2c8726dc43c4efcf17117
Barbara <bbrzepka@bgeo.es>
2021-01-15 11:26:27 +0100

    update fix on  config form fields

--- updates/34/34026/utils/dml.sql
+++ updates/34/34026/utils/dml.sql
@@ -84,3 +84,6 @@ INSERT INTO config_param_system VALUES ('admin_config_control_trigger', 'TRUE', 
 
 DELETE FROM sys_param_user WHERE id = 'basic_search_municipality_vdefault';
 DELETE FROM config_param_user WHERE parameter = 'basic_search_municipality_vdefault';
+
+UPDATE config_form_fields SET dv_parent_id = 'feature_id', dv_querytext = 'SELECT id, idval FROM (SELECT node_1 AS id, node_1 AS idval FROM arc UNION SELECT DISTINCT node_2 AS id, node_2 AS idval FROM arc)c WHERE id IS NOT NULL' WHERE formname = 'visit_singlevent' AND columnname = 'position_id';
+
