bbabe23a409b3ca8bc85a79710868f25435a6f88
Xavier Torret <xtorret@bgeo.es>
2021-01-23 22:51:16 +0100

    Bug fix when inport inp epanet files

--- updates/34/34027/ws/ddlview.sql
+++ updates/34/34027/ws/ddlview.sql
@@ -50,3 +50,34 @@ UNION
      JOIN vi_parent_arc a ON a.node_2::text = n.node_id::text
   WHERE n.sector_id = selector_sector.sector_id AND selector_sector.cur_user = "current_user"()::text)a;
 
+
+DROP VIEW vi_status;
+CREATE OR REPLACE VIEW vi_status AS 
+ SELECT rpt_inp_arc.arc_id,
+status
+   FROM selector_inp_result,
+    rpt_inp_arc
+  WHERE (rpt_inp_arc.status::text = 'CLOSED'::text OR rpt_inp_arc.status::text = 'OPEN'::text) AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text 
+  AND selector_inp_result.cur_user = "current_user"()::text AND rpt_inp_arc.epa_type::text = 'VALVE'::text
+UNION
+ SELECT rpt_inp_arc.arc_id,
+status   
+FROM selector_inp_result,
+    rpt_inp_arc
+  WHERE rpt_inp_arc.status::text = 'CLOSED'::text AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text AND rpt_inp_arc.epa_type::text = 'PUMP'::text
+UNION
+ SELECT rpt_inp_arc.arc_id,
+status
+   FROM selector_inp_result,
+    rpt_inp_arc
+  WHERE rpt_inp_arc.status::text = 'CLOSED'::text AND rpt_inp_arc.result_id::text = selector_inp_result.result_id::text AND selector_inp_result.cur_user = "current_user"()::text AND rpt_inp_arc.epa_type::text = 'PUMP'::text;
+
+
+
+CREATE TRIGGER gw_trg_vi_status
+  INSTEAD OF INSERT OR UPDATE OR DELETE
+  ON vi_status
+  FOR EACH ROW
+  EXECUTE PROCEDURE gw_trg_vi('vi_status');
+
+'
