4ea289c0f5fe53080e93bf7c938e8b52337d4466
Albert Bofill <abofill@bgeo.es>
2021-04-20 18:14:07 +0200

    Enhance plan_psector_x_connect triggers

--- ud/ftrg/ud_gw_trg_plan_gully_x_psector.sql
+++ ud/ftrg/ud_gw_trg_plan_gully_x_psector.sql
@@ -26,7 +26,8 @@ BEGIN
 
 	-- control if gully exists and it is link_class =  2
 	IF (SELECT count(*) FROM plan_psector_x_gully WHERE gully_id = NEW.gully_id) > 0 THEN
-		IF (SELECT link_geom FROM plan_psector_x_gully WHERE gully_id = NEW.gully_id ORDER BY link_geom limit 1) IS NULL THEN
+		IF (SELECT link_geom FROM plan_psector_x_gully WHERE gully_id = NEW.gully_id ORDER BY link_geom limit 1) IS NULL 
+		AND (SELECT state FROM gully WHERE gully_id = NEW.gully_id)=2 THEN
 		        EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
 			"data":{"message":"3082", "function":"2936","debug_msg":null}}$$);';
 		END IF;
@@ -34,12 +35,15 @@ BEGIN
 
 	SELECT gully.state, gully.arc_id INTO v_stateaux, v_arcaux FROM gully WHERE gully_id=NEW.gully_id;
 	
-	IF NEW.state IS NULL THEN
+	IF NEW.state IS NULL AND v_stateaux=1 THEN
 		NEW.state=0;
+	ELSIF NEW.state IS NULL AND v_stateaux=2 THEN
+		NEW.state=1;
 	END IF;
 	
 	IF NEW.state = 1 AND v_stateaux = 1 THEN
 		NEW.doable=false;
+        -- looking for arc_id state=2 closest
 	
 	ELSIF NEW.state = 0 AND v_stateaux=1 THEN
 		NEW.doable=false;
@@ -48,7 +52,7 @@ BEGIN
 	ELSIF v_stateaux=2 THEN
 		IF NEW.state = 0 THEN
 			EXECUTE 'SELECT gw_fct_getmessage($${"client":{"device":4, "infoType":1, "lang":"ES"},"feature":{},
-			"data":{"message":"3182", "function":"1130","debug_msg":'||OLD.psector_id||'}}$$);';
+			"data":{"message":"3182", "function":"1130","debug_msg":""}}$$);';
 		END IF;
 		NEW.doable=true;
 	END IF;
