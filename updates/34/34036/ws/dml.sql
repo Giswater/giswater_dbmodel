3e2bb076e86dd062a8043a5dbd049418c0241819
Barbara Rzepka <bbrzepka@bgeo.es>
2021-06-28 13:26:47 +0200

    config_form_fields for new inp views

--- updates/34/34036/ws/dml.sql
+++ updates/34/34036/ws/dml.sql
@@ -14,3 +14,96 @@ WHERE columnname = 'macrosector_id' AND formname = 'v_edit_inp_pump';
 
 UPDATE inp_controls_x_arc i SET sector_id = a.sector_id FROM arc a WHERE a.arc_id = i.arc_id;
 UPDATE inp_rules_x_arc i SET sector_id = a.sector_id FROM arc a WHERE a.arc_id = i.arc_id;
+
+INSERT INTO config_form_fields(formname, formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden)
+VALUES ('inp_curve', 'form_feature', 'sector_id', NULL, 'integer', 'combo', 'sector_id', FALSE, FALSE,
+TRUE, FALSE, 'SELECT sector_id as id, name as idval FROM sector WHERE sector_id IS NOT NULL ' , TRUE, TRUE,
+NULL,NULL,NULL, NULL,FALSE) ON CONFLICT (formname, formtype, columnname) DO NOTHING;
+
+INSERT INTO config_form_fields(formname, formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden)
+VALUES ('inp_pattern', 'form_feature', 'sector_id', NULL, 'integer', 'combo', 'sector_id', FALSE, FALSE,
+TRUE, FALSE, 'SELECT sector_id as id, name as idval FROM sector WHERE sector_id IS NOT NULL ' , TRUE, TRUE,
+NULL,NULL,NULL, NULL,FALSE) ON CONFLICT (formname, formtype, columnname) DO NOTHING;
+
+INSERT INTO config_form_fields(formname, formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden)
+VALUES ('inp_controls_x_arc', 'form_feature', 'sector_id', NULL, 'integer', 'combo', 'sector_id', TRUE, FALSE,
+TRUE, FALSE, 'SELECT sector_id as id, name as idval FROM sector WHERE sector_id IS NOT NULL ' , TRUE, FALSE,
+NULL,NULL,NULL, NULL,FALSE) ON CONFLICT (formname, formtype, columnname) DO NOTHING;
+
+INSERT INTO config_form_fields(formname, formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden)
+VALUES ('inp_rules_x_arc', 'form_feature', 'sector_id', NULL, 'integer', 'combo', 'sector_id', TRUE, FALSE,
+TRUE, FALSE, 'SELECT sector_id as id, name as idval FROM sector WHERE sector_id IS NOT NULL ' , TRUE, FALSE,
+NULL,NULL,NULL, NULL,FALSE) ON CONFLICT (formname, formtype, columnname) DO NOTHING;
+
+INSERT INTO config_form_fields(formname, formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden)
+SELECT 'v_edit_inp_curve', formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden
+FROM config_form_fields WHERE formname = 'inp_curve';
+
+INSERT INTO config_form_fields(formname, formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden)
+SELECT 'v_edit_inp_curve_value', formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden
+FROM config_form_fields WHERE formname = 'inp_curve' AND columnname IN ('curve_type', 'descript', 'sector_id');
+
+INSERT INTO config_form_fields(formname, formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden)
+SELECT 'v_edit_inp_curve_value', formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden
+FROM config_form_fields WHERE formname = 'inp_curve_value';
+
+INSERT INTO config_form_fields(formname, formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden)
+SELECT 'v_edit_inp_pattern', formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden
+FROM config_form_fields WHERE formname = 'inp_pattern' AND columnname not in ('pattern_type');
+
+INSERT INTO config_form_fields(formname, formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden)
+SELECT 'v_edit_inp_pattern_value', formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden
+FROM config_form_fields WHERE formname = 'inp_pattern' AND columnname not in ('pattern_type', 'pattern_id');
+
+INSERT INTO config_form_fields(formname, formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden)
+SELECT 'v_edit_inp_pattern_value', formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden
+FROM config_form_fields WHERE formname = 'inp_pattern_value' AND columnname not in ('pattern_id') and columnname not ilike '_%';
+
+INSERT INTO config_form_fields(formname, formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden)
+SELECT 'v_edit_inp_rules', formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden
+FROM config_form_fields WHERE formname = 'inp_rules_x_arc';
+
+
+INSERT INTO config_form_fields(formname, formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden)
+SELECT 'v_edit_inp_inp_controls', formtype, columnname, layoutorder, datatype, widgettype, label,  ismandatory, isparent, 
+iseditable, isautoupdate, dv_querytext, dv_orderby_id, dv_isnullvalue, 
+dv_parent_id, dv_querytext_filterc, widgetfunction, linkedaction,  hidden
+FROM config_form_fields WHERE formname = 'inp_controls_x_arc';
