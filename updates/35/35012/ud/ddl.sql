e2b25feca2af2ed18877d615ee22633267dc597a
xtorret@bgeo.es <xtorret@bgeo.es>
2021-09-05 19:26:10 +0200

    1D-2D SWMM / IBER - WiP

---
+++ updates/35/35012/ud/ddl.sql
@@ -0,0 +1,15 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+
+--2021/08/19
+
+
+-- 2021/09/03
+ALTER TABLE cat_grate RENAME efective_area TO effective_area;
