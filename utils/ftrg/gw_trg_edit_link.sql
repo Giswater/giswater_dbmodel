e490659183daebe4749d748737e801ee1934c141
Barbara Rzepka <bbrzepk@bgeo.es>
2023-04-06 15:48:25 +0200

    trg v_edit_link add limit 1 on if select

--- utils/ftrg/gw_trg_edit_link.sql
+++ utils/ftrg/gw_trg_edit_link.sql
@@ -461,7 +461,7 @@ BEGIN
 				"data":{"message":"3220", "function":"1116","debug_msg":null}}$$);';
 			END IF;
 		
-			IF (SELECT feature_id FROM link WHERE feature_id=NEW.feature_id AND link_id::text != NEW.link_id::text AND state = 1) IS NOT NULL THEN
+			IF (SELECT feature_id FROM link WHERE feature_id=NEW.feature_id AND link_id::text != NEW.link_id::text AND state = 1 LIMIT 1) IS NOT NULL THEN
 		
 				IF NEW.feature_type = 'CONNEC' THEN
 
@@ -558,7 +558,7 @@ BEGIN
 
 		ELSIF NEW.state = 1 THEN 
 
-			IF (SELECT feature_id FROM link WHERE feature_id=NEW.feature_id AND link_id::text != NEW.link_id::text AND state = 1) IS NOT NULL THEN
+			IF (SELECT feature_id FROM link WHERE feature_id=NEW.feature_id AND link_id::text != NEW.link_id::text AND state = 1 LIMIT 1) IS NOT NULL THEN
 
 				-- update psector
 				IF NEW.feature_type='CONNEC' THEN
