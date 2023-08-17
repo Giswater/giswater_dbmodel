954482d50740e862f6afa1d313cff4b3609a3edc
Albert Bofill <abofill@bgeo.es>
2023-08-17 09:20:12 +0200

    Bug fix on mapzones for planned connects and adding staticpressure on link (apply to 3.5)

---
+++ updates/35/35041/utils/ddl.sql
@@ -0,0 +1,11 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+
+SET search_path = SCHEMA_NAME ,public;
+
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"link", "column":"epa_type", "dataType":"character varying(16)", "isUtils":"False"}}$$);
+SELECT gw_fct_admin_manage_fields($${"data":{"action":"ADD","table":"link", "column":"is_operative", "dataType":"boolean", "isUtils":"False"}}$$);
