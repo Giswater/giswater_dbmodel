425d13929bda6b52693dcb9009c4fe575bb51663
Albert Bofill <abofill@bgeo.es>
2023-11-02 12:34:31 +0100

    Minor bug fix and update changelog

--- updates/35/35041/ud/dml.sql
+++ updates/35/35041/ud/dml.sql
@@ -15,3 +15,5 @@ INSERT INTO config_param_system(parameter, value, descript, label, isenabled,  p
 VALUES ('om_profile_nonpriority_statetype', '{"state_type":"", "extra_cost":0.1}', 
 'Features with defined state type won''t be prioritised to be chosen on a profile in case of overlaying conduiuts, instead it will have an additional path cost added to it''s length', 
 'Profile non priority state type', false, 'ud', 'json', 'linetext', false ) ON CONFLICT (parameter) DO NOTHING;
+
+UPDATE sys_param_user SET layoutorder=26 WHERE id='edit_node_ymax_vdefault';
