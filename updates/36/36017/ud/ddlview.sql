24c67a8ecb47ff066f0ef2cb53bf41ae9ad31a82
edgarfuste <34737125+edgarfuste@users.noreply.github.com>
2024-12-16 13:49:53 +0100

    Improve previous commit: schema reference "ed6923a"

--- updates/36/36017/ud/ddlview.sql
+++ updates/36/36017/ud/ddlview.sql
@@ -8,7 +8,6 @@ This version of Giswater is provided by Giswater Association
 SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 -- 16/12/24
-
 CREATE OR REPLACE VIEW vu_element_x_node
 AS SELECT element_x_node.id,
     element_x_node.node_id,
@@ -32,7 +31,6 @@ AS SELECT element_x_node.id,
      LEFT JOIN value_state_type ON element.state_type = value_state_type.id
      LEFT JOIN man_type_location ON man_type_location.location_type::text = element.location_type::text AND man_type_location.feature_type::text = 'ELEMENT'::text
      LEFT JOIN cat_element ON cat_element.id::text = element.elementcat_id::text;
-
 
 CREATE OR REPLACE VIEW vu_element_x_connec
 AS SELECT element_x_connec.id,
@@ -58,7 +56,6 @@ AS SELECT element_x_connec.id,
      LEFT JOIN man_type_location ON man_type_location.location_type::text = element.location_type::text AND man_type_location.feature_type::text = 'ELEMENT'::text
      LEFT JOIN cat_element ON cat_element.id::text = element.elementcat_id::text;
 
-
 CREATE OR REPLACE VIEW vu_element_x_arc
 AS SELECT element_x_arc.id,
     element_x_arc.arc_id,
@@ -78,6 +75,27 @@ AS SELECT element_x_arc.id,
     element.inventory
    FROM element_x_arc
      JOIN element ON element.element_id::text = element_x_arc.element_id::text
+     JOIN value_state ON element.state = value_state.id
+     LEFT JOIN value_state_type ON element.state_type = value_state_type.id
+     LEFT JOIN man_type_location ON man_type_location.location_type::text = element.location_type::text AND man_type_location.feature_type::text = 'ELEMENT'::text
+     LEFT JOIN cat_element ON cat_element.id::text = element.elementcat_id::text;
+
+CREATE OR REPLACE VIEW vu_element_x_gully
+AS SELECT element_x_gully.id,
+    element_x_gully.gully_id,
+    element_x_gully.element_id,
+    element.elementcat_id,
+    cat_element.descript,
+    element.num_elements,
+    value_state.name AS state,
+    value_state_type.name AS state_type,
+    element.observ,
+    element.comment,
+    element.location_type,
+    element.builtdate,
+    element.enddate
+   FROM element_x_gully
+     JOIN element ON element.element_id::text = element_x_gully.element_id::text
      JOIN value_state ON element.state = value_state.id
      LEFT JOIN value_state_type ON element.state_type = value_state_type.id
      LEFT JOIN man_type_location ON man_type_location.location_type::text = element.location_type::text AND man_type_location.feature_type::text = 'ELEMENT'::text
