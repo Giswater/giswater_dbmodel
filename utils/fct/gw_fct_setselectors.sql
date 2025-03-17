6a46b4dd89e0d9fdf18ddcbdbc18046129b16ac0
xtorret@bgeo.es <xtorret@bgeo.es>
2025-03-17 20:19:33 +0100

    fix(prev commit): Bug fix from prev commit

--- utils/fct/gw_fct_setselectors.sql
+++ utils/fct/gw_fct_setselectors.sql
@@ -393,7 +393,7 @@ BEGIN
 				
 			-- psector
 			DELETE FROM selector_psector WHERE psector_id NOT IN 
-			(SELECT psector_id FROM cat_dscenario WHERE active is true and expl_id IN (SELECT expl_id FROM selector_expl WHERE cur_user = current_user));
+			(SELECT psector_id FROM plan_psector WHERE active is true and expl_id IN (SELECT expl_id FROM selector_expl WHERE cur_user = current_user));
 		
 		END IF;
 
