a16d1414ccd6d50cd30011ae2df4616a6d387576
mguzman14 <mguzman@bgeo.es>
2025-06-27 11:20:01 +0200

    fix(v_edit_dma): bug fix when showing dmas with null expl_id

---
+++ updates/36/36022/ws/ddlview.sql
@@ -0,0 +1,56 @@
+/*
+This file is part of Giswater 3
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This version of Giswater is provided by Giswater Association
+*/
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+CREATE OR REPLACE view v_edit_dma AS
+SELECT d.dma_id,
+    d.name,
+    d.dma_type,
+    d.macrodma_id,
+    d.descript,
+    d.undelete,
+    d.expl_id,
+    d.minc,
+    d.maxc,
+    d.effc,
+    d.pattern_id,
+    d.link,
+    d.graphconfig::TEXT,
+    d.stylesheet::TEXT,
+    d.avg_press,
+    d.tstamp,
+    d.insert_user,
+    d.lastupdate,
+    d.lastupdate_user,
+    d.the_geom
+   FROM SCHEMA_NAME.selector_expl,
+    SCHEMA_NAME.dma d
+  WHERE (d.expl_id = selector_expl.expl_id AND d.active AND selector_expl.cur_user = "current_user"()::TEXT) 
+  UNION
+  SELECT 
+  	d.dma_id,
+    d.name,
+    d.dma_type,
+    d.macrodma_id,
+    d.descript,
+    d.undelete,
+    d.expl_id,
+    d.minc,
+    d.maxc,
+    d.effc,
+    d.pattern_id,
+    d.link,
+    d.graphconfig::TEXT,
+    d.stylesheet::TEXT,
+    d.avg_press,
+    d.tstamp,
+    d.insert_user,
+    d.lastupdate,
+    d.lastupdate_user,
+    d.the_geom
+  from SCHEMA_NAME.dma d where d.expl_id IS NULL
+  ORDER BY dma_id;
