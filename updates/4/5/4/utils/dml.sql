d7d82bf9c3474707ecc956dadee36f169d9cd3d5
Daniel Marín <dmarin@bgeo.es>
2025-10-29 12:13:28 +0100

    feat(sys_function): add new function gw_fct_getfeatureproperties to retrieve feature properties

--- updates/4/5/4/utils/dml.sql
+++ updates/4/5/4/utils/dml.sql
@@ -10,6 +10,7 @@ SET search_path = SCHEMA_NAME, public, pg_catalog;
 
 UPDATE sys_message SET log_level = 2 WHERE id = 4432;
 
+-- 29/10/2025
 UPDATE config_form_fields
 SET dv_querytext='SELECT r.result_id AS id, r.result_id AS idval FROM rpt_cat_result r JOIN v_ui_rpt_cat_result vr ON vr.result_id = r.result_id WHERE r.status = 2'
 WHERE formname='generic' AND formtype='epa_selector' AND columnname='result_name_compare' AND tabname='tab_result';
@@ -17,3 +18,6 @@ WHERE formname='generic' AND formtype='epa_selector' AND columnname='result_name
 UPDATE config_form_fields
 SET dv_querytext='SELECT r.result_id AS id, r.result_id AS idval FROM rpt_cat_result r JOIN v_ui_rpt_cat_result vr ON vr.result_id = r.result_id WHERE r.status = 2'
 WHERE formname='generic' AND formtype='epa_selector' AND columnname='result_name_show' AND tabname='tab_result';
+
+INSERT INTO sys_function (id, function_name, project_type, function_type, input_params, return_type, descript, sys_role, sample_query, "source", function_alias) 
+VALUES(3518, 'gw_fct_getfeatureproperties', 'utils', 'function', 'json', 'json', 'Function to get the properties of a feature.', NULL, NULL, 'core', NULL);
